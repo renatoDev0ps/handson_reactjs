a0f8dd58bdcdf693e528046d1eab0e5e64535dc1
renatodevops <renatodevops@gmail.com>
2019-08-05 14:59:00 -0300

    create Header

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,33 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Header from './Header'
 import {
-  Navbar,
-  NavbarBrand,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarToggler
-} from 'reactstrap'
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+const Genres = () => {
+  return <h1>Generos</h1>
+}
 
 function App() {
-  const [open, setOpen] = useState(false)
-  const toggle = () => {
-    setOpen(!open)
-  }
   return (
-    <div>
-      <Navbar color='light' light expand='md'>
-        <NavbarBrand>Minhas Séries</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={open} navbar>
-          <Nav className='ml-auto' navbar>
-            <NavItem>
-              <NavLink href='/'>Genêros</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route path='/genres' component={Genres} />
+      </div>
+    </Router>
   )
 }
 
