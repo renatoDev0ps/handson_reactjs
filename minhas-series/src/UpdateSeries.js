b26dfb7d9b84bac28188b660bad6c6230e94f608
renatodevops <renatodevops@gmail.com>
2019-08-06 14:09:36 -0300

    changes in series

--- minhas-series/src/UpdateSeries.js
+++ minhas-series/src/UpdateSeries.js
@@ -4,10 +4,13 @@ import { Redirect } from 'react-router-dom'
 import { Badge } from 'reactstrap'
 
 const UpdateSeries = ({ match }) => {
-  const [form, setForm] = useState({})
+  const [form, setForm] = useState({
+    name: ''
+  })
   const [success, setSuccess] = useState(false)
   const [mode, setMode] = useState('INFO')
   const [genres, setGenres] = useState([])
+  const [genreId, setGenreId] = useState('')
 
   const [data, setData] = useState({})
   useEffect(() => {
@@ -24,8 +27,13 @@ const UpdateSeries = ({ match }) => {
       .get('/api/genres')
       .then(res => {
         setGenres(res.data.data)
+        const genres = res.data.data
+        const found = genres.find(value => data.genre === value.name)
+        if(found) {
+          setGenreId(found.id)
+        }
       })
-  }, [])
+  }, [data])
 
   // custom header
   const masterHeader = {
@@ -37,21 +45,33 @@ const UpdateSeries = ({ match }) => {
     backgroundRepeat: 'no-repeat'
   }
 
+  const onChangeGenre = evt => {
+    setGenreId(evt.target.value)
+  }
   const onChange = field => evt => {
     setForm({
       ...form,
       [field]: evt.target.value
     })  
   }
+  const seleciona = value => () => {
+    setForm({
+      ...form,
+      status: value
+    })
+  }
   const save = () => {
     axios
-      .put('/api/series/' + match.params.id, form)
+      .put('/api/series/' + match.params.id, {
+        ...form,
+        genre_id: genreId
+      })
       .then(res => {
         setSuccess(true)
       })
   }
   if (success) {
-    //return <Redirect to='/series'/>
+    return <Redirect to='/series'/>
   }
   return (
     <div>
@@ -65,8 +85,8 @@ const UpdateSeries = ({ match }) => {
               <div className='col-8'>
                 <h1 className='font-wieght-light text-white'>{data.name}</h1>
                 <div className='lead text-white'>
-                  <Badge className='mr-1' color='success'>Assistido</Badge>
-                  <Badge className='mr-1' color='warning'>Para Assistir</Badge>
+                  { data.status === 'ASSISTIDO' && <Badge className='mr-1' color='success'>Assistido</Badge> }
+                  { data.status === 'PARA_ASSISTIR' && <Badge className='mr-1' color='warning'>Para Assistir</Badge> }
                   Gênero: {data.genre}
                 </div>
               </div>
@@ -83,8 +103,7 @@ const UpdateSeries = ({ match }) => {
         mode === 'EDIT' &&
         <div className='container'>
           <h1>Editar Série</h1>
-          <pre>{JSON.stringify(form)}</pre>
-          <button className='btn btn-warning mt-2' onClick={() => setMode('INFO')}>Cancel</button>
+          <button className='btn btn-primary mt-2' onClick={() => setMode('INFO')}>Cancel</button>
           <form>
             <div className='form-group'>
               <label htmlFor='nome'>Nome da Série</label>
@@ -96,9 +115,21 @@ const UpdateSeries = ({ match }) => {
             </div>
             <div className='form-group'>
               <label htmlFor='nome'>Gêneros</label>
-              <select className='form-control mb-2' onChange={onChange('genre_id')}>
-                { genres.map(genre => <option key={genre.id} value={genre.id} select={genre.id === form.genre}>{genre.name}</option>) }
+              <select className='form-control mb-2' onChange={onChangeGenre} value={genreId}>
+                { genres.map(genre => <option key={genre.id} value={genre.id}>{genre.name}</option>) }
               </select>
+            </div>
+            <div className='form-check'>
+              <input className='form-check-input' type='radio' checked={form.status === 'ASSISTIDO'} name='status' id='assistido' value='ASSISTIDO' onChange={seleciona('ASSISTIDO')}/>
+              <label className='form-check-label' htmlFor='assistido'>
+                Assitido
+              </label>
+            </div>
+            <div className='form-check mb-2'>
+              <input className='form-check-input' type='radio' checked={form.status === 'PARA_ASSISTIR'} name='status' id='paraAssistir' value='PARA_ASSISTIR' onChange={seleciona('PARA_ASSISTIR')}/>
+              <label className='form-check-label' htmlFor='paraAssistir'>
+                Para Assistir
+              </label>
             </div>
             <button type='button' className='btn btn-primary' onClick={save}>Save</button>
           </form>
